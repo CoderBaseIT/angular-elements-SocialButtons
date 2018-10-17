2bdcecbe6db9cf4cb3ea7695f7b9701b218e43ac
PierreNedelec <pierrenedelec46@gmail.com>
2018-10-17 22:54:08 +0200

    Update social-buttons.component.ts

--- src/app/social-buttons/social-buttons.component.ts
+++ src/app/social-buttons/social-buttons.component.ts
@@ -1,14 +1,17 @@
-import { Component, OnInit , Input} from '@angular/core';
+import { Component, OnInit , Input, ViewEncapsulation} from '@angular/core';
 @Component({
   selector: 'app-social-buttons',
   templateUrl: './social-buttons.component.html',
-  styleUrls: ['./social-buttons.component.css']
+  styleUrls: ['./social-buttons.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
-export class SocialButtonsComponent implements OnInit {
+export class SocialButtonsComponent implements OnChanges {
   @Input() provider: string;
   @Input() link: string;
+  public providerLink:string;
 
-  ngOnInit() {
+  ngOnChanges() {
+    this.providerLink = `fab fa-${this.provider}`;
   }
 
 
